f5e8fb4a6946b916b214d76c2efd2630d7b23301
reggmack <[200~21286023+reggmack@users.noreply.github.com~>
2024-02-17 15:00:14 -0600

    Clean up after git revert experiment

--- js/sketch.js
+++ js/sketch.js
@@ -1,25 +1,42 @@
-const container = document.createElement('div');
-container.classList.add('container');
+// Add container div for grid
+const container = document.createElement("div");
+container.classList.add("container");
 document.body.appendChild(container);
 
+// Create grid of varying size
 function createGrid(numberPerRow) {
   const total = numberPerRow * numberPerRow + numberPerRow;
   const mod = numberPerRow + 1;
   for (let i = 1; i < total; i++) {
-    const gridDiv = document.createElement('div');
-    gridDiv.classList.add('gridDiv');
+    const gridDiv = document.createElement("div");
+    gridDiv.classList.add("gridDiv");
     if (i % mod === 0) {
       gridDiv.style.cssText = "border: 0; height: 0; width: 100%";
     } else {
-      gridDiv.style.cssText = "border: 1px solid #767676; height: 25px; width: 25px";
+      gridDiv.style.cssText =
+        "border: 1px solid #767676; height: 25px; width: 25px";
     }
     container.appendChild(gridDiv);
   }
 }
 createGrid(16);
 
-const gridHover = document.querySelectorAll('.gridDiv')
-gridHover.forEach(box => box.addEventListener('mouseover', e => {
-    e.target.style.background = 'honeydew';
-}));
+// Add hover effect
+const gridHover = document.querySelectorAll(".gridDiv");
+gridHover.forEach((box) =>
+  box.addEventListener("mouseover", (e) => {
+    e.target.style.background = "honeydew";
+  })
+);
 
+// Add button to change grid size
+const gridButton = document.querySelector("#gridButton");
+gridButton.addEventListener("click", () => {
+  let gridSize;
+  do {
+    gridSize = parseInt(prompt("Chhose a grid size less than 100"), 10);
+    const clear = document.querySelector(".container");
+    clear.replaceChildren();
+    createGrid(gridSize);
+  } while (isNaN(gridSize) || gridSize > 100 || gridSize < 1);
+});
